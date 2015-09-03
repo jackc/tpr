7ac22c79fbdf09d25ffcd95c34eed1cc2859ea40
Jack Christensen <jack@jackchristensen.com>
2015-09-03 17:52:46 -0500

    Update gems

--- spec/support/factory_helper.rb
+++ spec/support/factory_helper.rb
@@ -3,7 +3,7 @@ require 'scrypt'
 
 module FactoryHelper
   def create_user attrs={}
-    defaults = {name: Faker::Internet.user_name, password: "password"}
+    defaults = {name: FFaker::Internet.user_name, password: "password"}
     attrs = defaults.merge(attrs)
     attrs[:password_salt] = Sequel::SQL::Blob.new "salt"
     password_digest = SCrypt::Engine.__sc_crypt attrs.delete(:password), attrs[:password_salt], 16384, 8, 1, 32
@@ -13,8 +13,8 @@ module FactoryHelper
 
   def create_feed attrs={}
     defaults = {
-      name: Faker::Lorem.sentence,
-      url: "http://localhost/#{Faker::Internet.domain_word}/#{Faker::Internet.domain_word}"
+      name: FFaker::Lorem.sentence,
+      url: "http://localhost/#{FFaker::Internet.domain_word}/#{FFaker::Internet.domain_word}"
     }
     attrs = defaults.merge(attrs)
     DB[:feeds].insert attrs
@@ -23,8 +23,8 @@ module FactoryHelper
   def create_item attrs={}
     attrs[:feed_id] ||= create_feed # not part of defaults hash so we don't create feed unless we need to
     defaults = {
-      title: Faker::Lorem.sentence,
-      url: Faker::Internet.http_url
+      title: FFaker::Lorem.sentence,
+      url: FFaker::Internet.http_url
     }
     attrs = defaults.merge(attrs)
     DB[:items].insert attrs
